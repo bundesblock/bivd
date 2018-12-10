b27f5b630f7d26a6a9ff2d69f7e30da23d0bb8e6
Eric Bolikowski <eric@norappweb.com>
2018-12-10 09:24:41 +0100

    Remove unused config entries

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,28 +1,11 @@
 module.exports = {
   siteTitle: 'BiVD',
   siteDescription: "BiVD",
+  siteUrl: "https://bivd-initiative.de/",
   authorName: 'BiVD',
-  twitterUsername: '_maxpou',
-  authorAvatar: '/images/avatar.jpeg',
   multilangPosts: true, // enable/disable flags in post lists
-  authorDescription: `
-  For the last decade, Maxence Poutord has worked with a variety of web technologies. He is currently focused on front-end development.
-  On his day to day job, he is working as a senior front-end engineer at VSware. He is also a frequent tech speaker and a mentor.
-  As a new digital nomad, he is living where the WIFI and sun is 😎 <br>
-  Do you want to know more? <a href="https://www.maxpou.fr/about" target="_blank">Visit my website!</a>
-  `,
-  siteUrl: 'https://maxpou.github.io/',
-  disqusSiteUrl: 'https://www.maxpou.fr/',
-  // Prefixes all links. For cases when deployed to maxpou.fr/gatsby-starter-morning-dew/
-  pathPrefix: '/gatsby-starter-morning-dew', // Note: it must *not* have a trailing slash.
-  siteCover: '/images/cover.jpg',
-  googleAnalyticsId: 'UA-67868977-1',
-  background_color: '#ffffff',
-  theme_color: '#222222',
-  display: 'minimal-ui',
   icon: 'src/assets/bivd-logo-square.png',
   postsPerPage: 6,
-  disqusShortname: 'maxpou',
   headerLinks: [
     {
       label: 'Initiative',
