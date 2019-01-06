8942a74ff6f09f9be28c51c7695a8e96b3b7598c
Eric Bolikowski <eric@norappweb.com>
2019-01-06 14:48:10 +0100

    Adapt header navigation and splash for low-width screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,9 +27,9 @@ const HeaderNav = styled.nav`
   overflow-x: auto;
   overflow-y: hidden;
   padding: 18px;
-  
+                                       
   height: 80px;
-  @media(max-width: 992px){
+  @media(min-width: 501px) and (max-width: 992px){             
     height: 140px;
   }
 `
@@ -47,6 +47,11 @@ const HeaderLink = styled(Link)`
   padding-right: 20px;
   min-width: 42px;
   z-index: 10;
+  ${props => props.hideOnSmallScreens &&  
+    `@media(max-width: 500px){
+      display: none;
+    }`
+  }
 `
 
 const Label = styled.span`
@@ -66,24 +71,86 @@ const LogoWrapper = styled.div`
     text-align: center;
     padding-bottom: 18px;
   }
+  @media(max-width: 500px){
+    text-align: left;
+    width: auto;
+  }
+`
+
+const Hamburger = styled.div`
+  display: none;
+  font-size: 30px;
+  cursor: pointer;
+  @media(max-width: 500px){
+    display: block;
+  }
+  :hover {
+    color: #0056b3;
+  }
+`
+
+const Sidenav = styled.div`
+  height: 100%; /* 100% Full-height */
+  width: ${props => props.sidebarOpen ? '120px;' : '0;'}; /* 0 width - change this with JavaScript */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Stay on top */
+  top: 80px; /* Stay at the top */
+  left: 0;
+  background-color: #eef4f9;
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 20px; /* Place content 60px from the top */
+  transition: 0.5s; /* 0.5 second transition effect to slide in the sidenav */
+  
+  a {
+    margin-bottom: 20px;
+  }
 `
 
 class Header extends React.Component {
+
+  state = {
+    sidebarOpen: false,
+  };
+
+  closeSidebar(){
+    this.setState({
+      sidebarOpen: false,
+    });
+  }
+
+  toggleSidebar(){
+    this.setState({
+      sidebarOpen: !this.state.sidebarOpen,
+    });
+  }
+
   render() {
     const { headerLinks } = this.props
+    const { sidebarOpen } = this.state;
 
     return (
-      <HeaderWrapper>
-        <HeaderNav>
-          <LogoWrapper><a href="/#top"><img src={bivdLogo} style={{ width: '162px', height: '44px' }} /></a></LogoWrapper>
+      <>
+        <Sidenav sidebarOpen={sidebarOpen}>
           {headerLinks.map((headerLink, i) => (
-            <HeaderLink to={headerLink.url} key={`header-link-${i}`}>
+            <HeaderLink to={headerLink.url} key={`header-link-${i}`} onClick={() => this.closeSidebar()}>
               <Label className="clearfix">{headerLink.label}</Label>
               <SubLabel>{headerLink.subLabel}</SubLabel>
             </HeaderLink>
           ))}
-        </HeaderNav>
-      </HeaderWrapper>
+        </Sidenav>
+        <HeaderWrapper>
+          <HeaderNav>
+            <LogoWrapper><a href="/#top"><img src={bivdLogo} style={{ width: '162px', height: '44px' }} /></a></LogoWrapper>
+            {headerLinks.map((headerLink, i) => (
+              <HeaderLink to={headerLink.url} hideOnSmallScreens={true} key={`header-link-${i}`}>
+                <Label className="clearfix">{headerLink.label}</Label>
+                <SubLabel>{headerLink.subLabel}</SubLabel>
+              </HeaderLink>
+            ))}
+            <Hamburger onClick={() => this.toggleSidebar()}>☰</Hamburger>
+          </HeaderNav>
+        </HeaderWrapper>
+      </>
     )
   }
 }
